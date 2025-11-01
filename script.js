0ca9c55dff0daaccf1ee053f2186e75d6cc8bb2d
Nikita Goyal <nikiiigoyal@gmail.com>
2025-11-01 12:05:11 +0530

    Add the concept of Rect Area Lights

--- script.js
+++ script.js
@@ -8,24 +8,25 @@ import { cameraProjectionMatrix } from 'three/tsl';
 let canvas = document.getElementById("#threejs")
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight,1,1000);
-camera.position.set(0,5,-15)
+camera.position.set(0, 5, -15)
 
-let renderer
+let renderer;
 
 THREE.RectAreaLightNode.setLTC( RectAreaLightTexturesLib.init() );
 
-				renderer = new THREE.WebGPURenderer( { antialias: true } );
-				renderer.setPixelRatio( window.devicePixelRatio );
-				renderer.setSize( window.innerWidth, window.innerHeight );
-				renderer.setAnimationLoop( animation );
-				document.body.appendChild( renderer.domElement );
+renderer = new THREE.WebGPURenderer( { antialias: true } );
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animation );
+document.body.appendChild( renderer.domElement );
 console.log("Renderer is using canvas element:", renderer.domElement === canvas);
 
+// geometry & material
 const geoFloor = new THREE.BoxGeometry(2000,0.1,2000)
-const matStdFloor = new THREE.MeshStandardMaterial({color: 0xbcbcbc,roughness: 0.1 ,metalness: 0});
+const matStdFloor = new THREE.MeshStandardMaterial({color:0xbcbcbc,roughness: 0.1 ,metalness: 0});
 const meshgeoFloor = new THREE.Mesh(geoFloor,matStdFloor);
 scene.add(meshgeoFloor);
-console.log(meshgeoFloor)
+console.log("meshgeoflorr:",meshgeoFloor)
 
 const geoKnot = new THREE.TorusKnotGeometry(1.5,0.5,200,16)
 const matKnot = new THREE.MeshStandardMaterial({color: 0xffffff,roughness:0,metalness:0})
@@ -34,21 +35,23 @@ meshKnot.position.set(0,5,0)
 scene.add(meshKnot)
 console.log("Torus Knot added at:", meshKnot.position);
 
-const rectLight1 = new THREE.RectAreaLight(0xffffff,5,4,10)
-rectLight1.position.set(-5,5,5)
-scene.add(rectLight1)
+// red blue green lights
+const rectLight1 = new THREE.RectAreaLight( 0xff0000, 5, 4, 10 );
+				rectLight1.position.set( - 5, 5, 5 );
+				scene.add( rectLight1 );
 
-const rectLight2 = new THREE.RectAreaLight(0xffffff,5,4,10)
-rectLight1.position.set(0,5,5)
-scene.add(rectLight2)
+				const rectLight2 = new THREE.RectAreaLight( 0x00ff00, 5, 4, 10 );
+				rectLight2.position.set( 0, 5, 5 );
+				scene.add( rectLight2 );
 
-const rectLight3 = new THREE.RectAreaLight(0xffffff,5,4,10)
-rectLight1.position.set(5,5,5)
-scene.add(rectLight3)
+				const rectLight3 = new THREE.RectAreaLight( 0x0000ff, 5, 4, 10 );
+				rectLight3.position.set( 5, 5, 5 );
+				scene.add( rectLight3 );
 
 scene.add(new RectAreaLightHelper(rectLight1))
 scene.add(new RectAreaLightHelper(rectLight2))
 scene.add(new RectAreaLightHelper(rectLight3))
+
 
 const controls = new OrbitControls(camera,canvas)
 controls.target.copy(meshKnot.position)
